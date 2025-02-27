0860d58cb8127e09daf9e9d4b18569593d3462b0
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2025-02-27 18:57:07 -0300

    e

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import {create} from 'express-handlebars'
 import passport from 'passport'
 import initalizatePassport from './config/passport.config.js'
 import MongoStore from 'connect-mongo'
-import sessionRouter from './routes/sessions.routes.js'
-import productRouter from './routes/products.routes.js'
-import cartRouter from './routes/carts.routes.js'
+import indexRouter from './routes/index.routes.js'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 
 const app = express()
 const PORT = 5000
 const hbs = create()
+app.use(cors())
 
 //const fileStorage = new FileStore(session)
 app.use(express.json())
@@ -48,14 +48,13 @@ mongoose.connect("mongodb+srv://benjaartunduaga00:07AywxOhOrJdyRhH@cluster0.0mjf
     app.engine('handlebars', hbs.engine)
     app.set('view engine', 'handlebars')
     app.set('views', path.join(__dirname, 'views')) //Concateno evitando erroes de / o \
+    
+    
     //Rutas
-    app.use('/public', express.static(__dirname + '/public')) //Concateno rutas
-    app.use('/api/sessions', sessionRouter)
-    app.use('/api/products', productRouter)
-    app.use('/api/carts', cartRouter)
-    app.get('/', (req, res)=>{
-        res.status(200).send("Hola desde iniciooo")
-    })
+    app.use('/', indexRouter)
+
+
+    
     app.listen(PORT, () => {
         console.log(`Server on port ${PORT}`)
     })
